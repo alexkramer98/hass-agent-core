79edaa532f5101d6febe90695689fec0c161d79b
alex <alex@keicreations.nl>
2024-03-04 00:55:31 +0100

    add support for satelites

--- src/services/ConversationHandler.ts
+++ src/services/ConversationHandler.ts
@@ -3,36 +3,59 @@ import type { Intent, IntentVars } from "../interfaces";
 import crypto from "crypto";
 
 export default class ConversationHandler {
-  private readonly messages: {
-    [id: string]: {
-      intent: Intent;
-      vars: IntentVars;
-      answerKey: string;
-      deviceId?: string;
-    };
-  } = {};
+  private readonly activeConversations: {
+    intent: Intent;
+    vars: IntentVars;
+    answerKey: string;
+    deviceId?: string;
+    messageId?: string;
+  }[] = [];
   private readonly inputParser: InputParser;
 
   public constructor(inputParser: InputParser) {
     this.inputParser = inputParser;
   }
 
+  private findBy(deviceId?: string, messageId?: string) {
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    let index = -1;
+
+    if (deviceId !== undefined) {
+      index = this.activeConversations.findIndex(
+        (item) => item.deviceId === deviceId,
+      );
+    } else if (messageId !== undefined) {
+      console.log("hier");
+      index = this.activeConversations.findIndex(
+        (item) => item.messageId === messageId,
+      );
+      console.log(
+        this.activeConversations,
+        index,
+        this.activeConversations[index],
+      );
+    }
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    if (index !== -1) {
+      console.log("found");
+      return this.activeConversations.splice(index, 1)[0];
+    }
+
+    return undefined;
+  }
+
   public handle(message: string, deviceId?: string, messageId?: string) {
     let intent: Intent | undefined = undefined;
     let vars: IntentVars = {};
 
-    if (messageId !== undefined) {
-      const conversation = this.messages[messageId];
-      if (conversation) {
-        intent = conversation.intent;
-        vars = conversation.vars;
-        vars[conversation.answerKey] = message
-          .toLowerCase()
-          .replaceAll(/\W/g, "");
+    const conversation = this.findBy(deviceId, messageId);
 
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        delete this.messages[messageId];
-      }
+    if (conversation !== undefined) {
+      intent = conversation.intent;
+      vars = conversation.vars;
+      vars[conversation.answerKey] = message
+        .toLowerCase()
+        .replaceAll(/\W/g, "");
     }
 
     if (intent === undefined) {
@@ -55,14 +78,17 @@ export default class ConversationHandler {
         msg: response.message,
       };
     }
-    const newMessageId = crypto.randomUUID();
 
-    this.messages[newMessageId] = {
+    const newMessageId =
+      deviceId === undefined ? undefined : crypto.randomUUID();
+
+    this.activeConversations.push({
       intent,
       vars,
       answerKey: response.answerKey,
       deviceId,
-    };
+      messageId: newMessageId,
+    });
 
     return {
       id: newMessageId,
