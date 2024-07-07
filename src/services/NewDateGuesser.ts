3d440afa49d2ea29015625e0e6f364ad2ae76ac6
alex <alex@keicreations.nl>
2024-07-08 01:59:58 +0200

    tests, work

--- src/services/NewDateGuesser.ts
+++ src/services/NewDateGuesser.ts
@@ -1,24 +1,71 @@
+// eslint-disable-next-line simple-import-sort/imports
 import "dayjs/locale/nl";
-
+import type { Dayjs } from "dayjs";
 import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import localizedFormatPlugin from "dayjs/plugin/localizedFormat";
 import updateLocalePlugin from "dayjs/plugin/updateLocale";
 import weekdayPlugin from "dayjs/plugin/weekday";
+import weekOfYearPlugin from "dayjs/plugin/weekOfYear";
 
+// eslint-disable-next-line import/no-named-as-default-member
 dayjs.extend(updateLocalePlugin);
-
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.extend(customParseFormat);
+// eslint-disable-next-line import/no-named-as-default-member
 dayjs.locale("nl");
 dayjs.updateLocale("nl", {
   weekStart: 1,
 });
+
+// eslint-disable-next-line import/no-named-as-default-member
 dayjs.extend(localizedFormatPlugin);
+// eslint-disable-next-line import/no-named-as-default-member
 dayjs.extend(weekdayPlugin);
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.extend(weekOfYearPlugin);
 
 const FULL_HOUR = 60;
 const HALF_HOUR = 30;
 const QUARTER_HOUR = 15;
+const HALF_DAY = 12;
+const FULL_DAY = 24;
 
+const MONTHS = [
+  "januari",
+  "februari",
+  "maart",
+  "april",
+  "mei",
+  "juni",
+  "juli",
+  "augustus",
+  "september",
+  "oktober",
+  "november",
+  "december",
+];
+
+const DAYS = [
+  "maandag",
+  "dinsdag",
+  "woensdag",
+  "donderdag",
+  "vrijdag",
+  "zaterdag",
+  "zondag",
+];
+
+const ABSOLUTE_DATE_REGEX =
+  /^(?:op )?(?<day>\d\d?) (?<month>april|augustus|december|februari|januari|juli|juni|maart|mei|november|oktober|september)(?: (?<year>\d{4}))?/v;
+
+const RELATIVE_DATE_REGEX =
+  /^(?:(?<weekday_d>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag) ?)?(?:over|in|(?:op )?(?<weekday_a>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag) ?) (?:(?<years>\d\d?) jaar)?(?: en |, | )?(?:(?<months>\d\d?) maand)?(?: ?(?:in|op) de (?<week>\d)e (?:week (?:van (?<month>april|augustus|februari|januari|juli|juni|maart|mei|novemberdecember|oktober|september))?)?(?:(?:op )?(?<weekday_b>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag) ?)?)?(?: en |, | )?(?:(?<weeks>\d\d?) week(?: op ?(?<weekday_c>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag) ?)?)?(?: en |, | )?(?:(?<days>\d\d?) dag)?(?: en |, | )?(?:(?<hours>\d\d?) uur)?(?: en |, | )?(?:(?<minutes>\d\d?) minuut)?(?: en |, | )?/v;
+
+//   todo: miss daypart niet meer nodig, want timeparser gebruikt geen regex
+// /^(?:(?<weekday_d>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag) ?(?<daypart_d>avond|middag|nacht|ochtend)?)?(?:over|in|(?:op )?(?<weekday_a>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag)(?: ?(?<daypart_a>avond|middag|nacht|ochtend))?) (?:(?<years>\d\d?) jaar)?(?: en |, | )?(?:(?<months>\d\d?) maand)?(?: ?(?:in|op) de (?<week_absolute>\d)e (?:week (?:van (?<month>april|augustus|februari|januari|juli|juni|maart|mei|novemberdecember|oktober|september))?)?(?:(?:op )?(?<weekday_b>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag)(?: ?(?<daypart_b>avond|middag|nacht|ochtend))?)?)?(?: en |, | )?(?:(?<weeks_relative>\d\d?) week(?: op ?(?<weekday_c>dinsdag|donderdag|maandag|vrijdag|woensdag|zaterdag|zondag)(?: ?(?<daypart_c>avond|middag|nacht|ochtend))?)?)?(?: en |, | )?(?:(?<days>\d\d?) dag)?(?: en |, | )?(?:(?<hours>\d\d?) uur)?(?: en |, | )?(?:(?<minutes>\d\d?) minuut)?(?: en |, | )?/v;
 export default class NewDateGuesser {
+  //   todo: .set naar .unit
   private normalizeInput(result: string): string {
     const replacements = {
       eerste: "1e",
@@ -46,6 +93,7 @@ export default class NewDateGuesser {
       "1 half uur": "30 minuten",
       "anderhalf uur": "1 uur en 30 minuten",
       "3 kwartier": "45 minuten",
+      "1 kwartier": "15 minuten",
       kwartier: "15 minuten",
       "half uur": "30 minuten",
       minuten: "minuut",
@@ -65,9 +113,10 @@ export default class NewDateGuesser {
   }
 
   private splitInput(input: string) {
-    const regex =
-      /(?!$) ?(?:om )?(?:\d\d? )?(?:(?:half|(?:kwart )?voor(?: half)?|(?:kwart )?over(?: half)?) )?(?:\d\d?)?(?:uur)?(?: in de (?:avond|middag|nacht|ochtend))?$/v;
+    const regex = //   todo: deze moet niet cijfers jatten uit "2 december 2025"
+      /(?!$)(?:(?:avond|middag|nacht|ochtend) )?(?:om )?(?:\d\d? )?(?:(?:half|(?:kwart )?voor(?: half)?|(?:kwart )?over(?: half)?) )?(?:\d\d?)?(?:uur)?(?: in de (?:avond|middag|nacht|ochtend))?$/v;
 
+    // /(?!$) ?(?:om )?(?:\d\d? )?(?:(?:half|(?:kwart )?voor(?: half)?|(?:kwart )?over(?: half)?) )?(?:\d\d?)?(?:uur)?(?: in de (?:avond|middag|nacht|ochtend))?$/v;
     const match = regex.exec(input);
 
     return {
@@ -76,32 +125,15 @@ export default class NewDateGuesser {
     };
   }
 
-  private getNumberOrUndefined(input?: string) {
+  private parseNumber(input?: string) {
     const number = Number(input);
 
     return Number.isNaN(number) ? undefined : number;
   }
 
-  private parsePeriod(input: string) {
-    let period: "afternoon" | "evening" | "morning" | "night" | undefined =
-      undefined;
-
-    if (input.includes("nacht")) {
-      period = "night";
-    } else if (input.includes("ochtend")) {
-      period = "morning";
-    } else if (input.includes("middag")) {
-      period = "afternoon";
-    } else if (input.includes("avond")) {
-      period = "evening";
-    }
-
-    return { period };
-  }
-
   private parseFullHour(input: string) {
     return {
-      hours: this.getNumberOrUndefined(
+      hours: this.parseNumber(
         /(?<hours>\d\d?) uur/v.exec(input)?.groups?.hours,
       ),
 
@@ -110,35 +142,42 @@ export default class NewDateGuesser {
   }
 
   private parseAfterHour(input: string) {
-    const testResult =
+    const result =
       /(?:(?<minutes>\d\d?)|kwart) over (?:half )?(?<hours>\d\d?)/v.exec(
         input,
       )?.groups;
 
-    const hours = this.getNumberOrUndefined(testResult?.hours);
+    let hours = this.parseNumber(result?.hours);
+    let minutes = this.parseNumber(result?.minutes);
 
-    let minutes = this.getNumberOrUndefined(testResult?.minutes);
-
-    if (minutes !== undefined && input.includes("half")) {
+    if (
+      minutes !== undefined &&
+      hours !== undefined &&
+      input.includes("half")
+    ) {
       minutes += HALF_HOUR;
+      hours -= 1;
     }
 
     if (input.includes("kwart")) {
       minutes = QUARTER_HOUR;
     }
 
-    return { hours, minutes };
+    return {
+      hours,
+      minutes,
+    };
   }
 
   // eslint-disable-next-line max-statements
   private parseBeforeHour(input: string) {
-    const testResult =
+    const result =
       /(?:(?<minutes>\d\d?)|kwart) voor (?:half )?(?<hours>\d\d?)/v.exec(
         input,
       )?.groups;
 
-    let hours = this.getNumberOrUndefined(testResult?.hours);
-    let minutes = this.getNumberOrUndefined(testResult?.minutes);
+    let hours = this.parseNumber(result?.hours);
+    let minutes = this.parseNumber(result?.minutes);
 
     if (hours !== undefined) {
       hours -= 1;
@@ -156,64 +195,173 @@ export default class NewDateGuesser {
       minutes = FULL_HOUR - QUARTER_HOUR;
     }
 
-    return { hours, minutes };
+    return {
+      hours,
+      minutes,
+    };
   }
 
   private parseHalfHour(input: string) {
-    let hours = undefined;
+    const result = /half (?<hours>\d\d?)/v.exec(input)?.groups;
 
-    const testResult = /half (?<hours>\d\d?)/v.exec(input)?.groups;
-
-    hours = this.getNumberOrUndefined(testResult?.hours);
+    let hours = this.parseNumber(result?.hours);
 
     if (hours !== undefined) {
       hours -= 1;
     }
 
-    return { hours, minutes: HALF_HOUR };
+    return {
+      hours,
+      minutes: HALF_HOUR,
+    };
   }
 
-  private parseAbsoluteTime(input: string) {
+  // eslint-disable-next-line complexity,max-statements,sonarjs/cognitive-complexity
+  private parseAbsoluteTime(start: Dayjs, input: string) {
+    let hours = undefined;
+    let minutes = undefined;
+
     if (input.includes("uur")) {
-      return {
-        ...this.parseFullHour(input),
-        ...this.parsePeriod(input),
-      };
+      ({ hours, minutes } = this.parseFullHour(input));
+    } else if (input.includes("over")) {
+      ({ hours, minutes } = this.parseAfterHour(input));
+    } else if (input.includes("voor")) {
+      ({ hours, minutes } = this.parseBeforeHour(input));
+    } else if (input.includes("half")) {
+      ({ hours, minutes } = this.parseHalfHour(input));
     }
 
-    if (input.includes("over")) {
-      return {
-        ...this.parseAfterHour(input),
-        ...this.parsePeriod(input),
-      };
+    if (hours === undefined) {
+      hours = 0;
     }
 
-    if (input.includes("voor")) {
-      return {
-        ...this.parseBeforeHour(input),
-        ...this.parsePeriod(input),
-      };
+    if (minutes === undefined) {
+      minutes = 0;
     }
 
-    if (input.includes("half")) {
-      return {
-        ...this.parseHalfHour(input),
-        ...this.parsePeriod(input),
-      };
+    // expect 0-8 and 12 to be in the afternoon/evening, unless otherwise specified
+    if (
+      input.includes("middag") ||
+      input.includes("avond") || // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+      (!input.includes("nacht") && !input.includes("ochtend") && hours <= 8) || // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+      hours === 12
+    ) {
+      hours += HALF_DAY;
     }
 
-    return {
-      hours: undefined,
-      minutes: undefined,
-      ...this.parsePeriod(input),
-    };
+    if (hours >= FULL_DAY) {
+      hours = 0;
+    }
+
+    return start.hour(hours).minute(minutes);
+  }
+
+  private parseAbsoluteDate(start: Dayjs, input: string) {
+    const result = ABSOLUTE_DATE_REGEX.exec(input)?.groups;
+
+    const day = this.parseNumber(result?.day);
+    const year = this.parseNumber(result?.year);
+    const month = result?.month;
+
+    let newDate = start.clone();
+
+    if (day !== undefined) {
+      newDate = newDate.set("date", day);
+    }
+
+    if (month !== undefined) {
+      newDate = newDate.set("months", MONTHS.indexOf(month));
+    }
+
+    if (year !== undefined) {
+      newDate = newDate.set("years", year);
+    }
+
+    return newDate;
+  }
+
+  private parseRelativeDate(start: Dayjs, input: string) {
+    const result = RELATIVE_DATE_REGEX.exec(input)?.groups;
+
+    const years = this.parseNumber(result?.years);
+    const months = this.parseNumber(result?.months);
+    const weeks = this.parseNumber(result?.weeks);
+    const days = this.parseNumber(result?.days);
+    const hours = this.parseNumber(result?.hours);
+    const minutes = this.parseNumber(result?.minutes);
+    const month = result?.month;
+    const week = this.parseNumber(result?.week);
+    const weekday =
+      result?.weekday_a ??
+      result?.weekday_b ??
+      result?.weekday_c ??
+      result?.weekday_d;
+
+    let newDate = start.clone().hour(0).minute(0);
+
+    if (years !== undefined) {
+      newDate = newDate.add(years, "years");
+    }
+
+    if (months !== undefined) {
+      newDate = newDate.add(months, "months");
+    }
+
+    if (weeks !== undefined) {
+      newDate = newDate.add(weeks, "weeks");
+    }
+
+    if (days !== undefined) {
+      newDate = newDate.add(days, "days");
+    }
+
+    if (hours !== undefined) {
+      newDate = newDate.add(hours, "hours");
+    }
+
+    if (minutes !== undefined) {
+      newDate = newDate.add(minutes, "minutes");
+    }
+
+    if (month !== undefined) {
+      newDate = newDate.month(MONTHS.indexOf(month));
+    }
+
+    if (week !== undefined) {
+      newDate = newDate.week(week);
+    }
+
+    if (weekday !== undefined) {
+      newDate = newDate.weekday(DAYS.indexOf(weekday));
+    }
+
+    return newDate;
   }
 
   public guess(input: string) {
     const normalizedInput = this.normalizeInput(input);
 
-    const { absoluteTime, remainder } = this.splitInput(normalizedInput);
+    const start = dayjs()
+      .set("years", 2000)
+      .set("months", 0)
+      .set("date", 1)
+      .set("hours", 0)
+      .set("minutes", 0)
+      .set("seconds", 0);
 
-    console.log(this.parseAbsoluteTime(absoluteTime));
+    // const { absoluteTime, remainder } = this.splitInput(normalizedInput);
+    const time = this.parseAbsoluteTime(start, normalizedInput);
+
+    // todo: als niet remainder, bouw absolute tijd en baseer op vandaag
+    // er kunnen regexes zijn die dingen matchen die ze niet mogen matchen, dus remainder lijkt noodzakelijk zodat er een full test gedaan kan worden
+    if (ABSOLUTE_DATE_REGEX.test(normalizedInput)) {
+      return this.parseAbsoluteDate(time, normalizedInput);
+    }
+
+    if (RELATIVE_DATE_REGEX.test(normalizedInput)) {
+      return this.parseRelativeDate(time, normalizedInput);
+    }
+
+    return undefined;
   }
 }
--- src/test.ts
+++ src/test.ts
@@ -1,26 +1,348 @@
+import type { Dayjs } from "dayjs";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import localizedFormatPlugin from "dayjs/plugin/localizedFormat";
+import updateLocalePlugin from "dayjs/plugin/updateLocale";
+import weekdayPlugin from "dayjs/plugin/weekday";
+import weekOfYearPlugin from "dayjs/plugin/weekOfYear";
+
 import NewDateGuesser from "./services/NewDateGuesser";
+
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.extend(updateLocalePlugin);
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.extend(customParseFormat);
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.locale("nl");
+dayjs.updateLocale("nl", {
+  weekStart: 1,
+});
+
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.extend(localizedFormatPlugin);
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.extend(weekdayPlugin);
+// eslint-disable-next-line import/no-named-as-default-member
+dayjs.extend(weekOfYearPlugin);
 
 const guesser = new NewDateGuesser();
 
-const input = `om 3 uur 's middags
-om 4 uur 's nachts
-om 8 uur 's avonds
-7 uur in de ochtend
-om half 6
-om 5 uur
-om kwart voor 7
-om kwart over 10
-om 20 over 3
-om tien voor half 8
-10 over 2 in de nacht
-om 7 over 3
-tien over half 8
-om 20 over 9
-om middernacht
-om 6 voor zeven
-om vijf voor half 3`;
+const getDate = (): Dayjs =>
+  // -------------------
+  dayjs()
+    .set("years", 2000)
+    .set("months", 0)
+    .set("date", 1)
+    .set("hours", 0)
+    .set("minutes", 0)
+    .set("seconds", 0);
 
-for (const item of input.split("\n")) {
-  console.log("input:", item);
-  guesser.guess(item);
+const tests = {
+  "4 februari om half 4": getDate()
+    .set("date", 4)
+    .set("months", 1)
+    .set("hours", 15)
+    .set("minutes", 30),
+
+  "10 maart om kwart voor 6": getDate()
+    .set("date", 10)
+    .set("months", 2)
+    .set("hours", 17)
+    .set("minutes", 45),
+
+  "1 januari 2025 om middernacht": getDate()
+    .set("year", 2025)
+    .set("date", 1)
+    .set("months", 0)
+    .set("hours", 0)
+    .set("minutes", 0),
+
+  "15 april om 10 uur 's ochtends": getDate()
+    .set("date", 15)
+    .set("months", 3)
+    .set("hours", 10)
+    .set("minutes", 0),
+
+  "30 juni om kwart over 2 's middags": getDate()
+    .set("date", 30)
+    .set("months", 5)
+    .set("hours", 14)
+    .set("minutes", 15),
+
+  "20 juli 2024 om 5 over half 8": getDate()
+    .set("year", 2024)
+    .set("date", 20)
+    .set("months", 6)
+    .set("hours", 19)
+    .set("minutes", 35),
+
+  "25 augustus om 3 uur 's nachts": getDate()
+    .set("date", 25)
+    .set("months", 7)
+    .set("hours", 3)
+    .set("minutes", 0),
+
+  "10 september om 11 uur": getDate()
+    .set("date", 10)
+    .set("months", 8)
+    .set("hours", 11)
+    .set("minutes", 0),
+
+  "5 oktober om 20 voor 4 's middags": getDate()
+    .set("date", 5)
+    .set("months", 9)
+    .set("hours", 15)
+    .set("minutes", 40),
+
+  "31 december 2024 om kwart voor 12": getDate()
+    .set("year", 2024)
+    .set("date", 31)
+    .set("months", 11)
+    .set("hours", 11)
+    .set("minutes", 45),
+
+  "2 februari om kwart over 3 's middags": getDate()
+    .set("date", 2)
+    .set("months", 1)
+    .set("hours", 15)
+    .set("minutes", 15),
+
+  "8 maart om 20 over 8 's ochtends": getDate()
+    .set("date", 8)
+    .set("months", 2)
+    .set("hours", 8)
+    .set("minutes", 20),
+
+  "14 april 2024 om kwart over 7": getDate()
+    .set("year", 2024)
+    .set("date", 14)
+    .set("months", 3)
+    .set("hours", 19)
+    .set("minutes", 15),
+
+  "22 mei om 10 uur 's avonds": getDate()
+    .set("date", 22)
+    .set("months", 4)
+    .set("hours", 22)
+    .set("minutes", 0),
+
+  "19 juni om half 2": getDate()
+    .set("date", 19)
+    .set("months", 5)
+    .set("hours", 13)
+    .set("minutes", 30),
+
+  "19 juni om zeven voor half 9 in de ochtend": getDate()
+    .set("date", 19)
+    .set("months", 5)
+    .set("hours", 8)
+    .set("minutes", 23),
+
+  "7 juli om kwart voor 9 's ochtends": getDate()
+    .set("date", 7)
+    .set("months", 6)
+    .set("hours", 8)
+    .set("minutes", 45),
+
+  "13 augustus 2024 om kwart over 5 in de ochtend": getDate()
+    .set("year", 2024)
+    .set("date", 13)
+    .set("months", 7)
+    .set("hours", 5)
+    .set("minutes", 15),
+
+  "27 september om 6 uur 's middags": getDate()
+    .set("date", 27)
+    .set("months", 8)
+    .set("hours", 18)
+    .set("minutes", 0),
+
+  "9 oktober om kwart over 11 in de avond": getDate()
+    .set("date", 9)
+    .set("months", 9)
+    .set("hours", 23)
+    .set("minutes", 15),
+
+  "16 november 2024 om 2 uur 's middags": getDate()
+    .set("year", 2024)
+    .set("date", 16)
+    .set("months", 10)
+    .set("hours", 14)
+    .set("minutes", 0),
+
+  "24 december om 9 uur 's ochtends": getDate()
+    .set("date", 24)
+    .set("months", 11)
+    .set("hours", 9)
+    .set("minutes", 0),
+
+  "11 januari om half 6": getDate()
+    .set("date", 11)
+    .set("months", 0)
+    .set("hours", 17)
+    .set("minutes", 30),
+
+  "5 mei om 3 uur 's middags": getDate()
+    .set("date", 5)
+    .set("months", 4)
+    .set("hours", 15)
+    .set("minutes", 0),
+
+  "18 juni om kwart voor 4 's middags": getDate()
+    .set("date", 18)
+    .set("months", 5)
+    .set("hours", 15)
+    .set("minutes", 45),
+
+  // -------------------
+
+  "over 3 uur": getDate().set("hours", 3),
+
+  "over 10 minuten": getDate().set("minutes", 10),
+
+  "over een kwartier": getDate().set("minutes", 15),
+
+  "over een half uur": getDate().set("minutes", 30),
+
+  "over drie kwartier": getDate().set("minutes", 45),
+
+  "in een uur": getDate().set("hours", 1),
+
+  "over 2 dagen": getDate().set("date", 3),
+
+  "over 6 maanden": getDate().set("months", 6),
+
+  "over 10 jaar": getDate().set("years", 2010),
+
+  "in 5 minuten": getDate().set("minutes", 5),
+
+  "in anderhalf uur": getDate().set("hours", 1).set("minutes", 30),
+
+  "over 2 jaar, 3 maanden en 2 weken, 1 dag, 4 uur en 30 minuten": getDate()
+    .set("years", 2002)
+    .set("months", 3)
+    .week(2)
+    .set("date", 1)
+    .set("hours", 4)
+    .set("minutes", 30),
+
+  "over 1 jaar en 2 maanden": getDate().set("years", 2001).set("months", 2),
+
+  "over 3 dagen en 6 en een half uur": getDate()
+    .set("date", 4)
+    .set("hours", 6)
+    .set("minutes", 30),
+
+  "over 3 weken": getDate().week(4),
+
+  "over een maand en 1 week": getDate().set("months", 1).week(1),
+
+  "in 2 weken op maandagavond": getDate()
+    .set("weeks", 2)
+    .set("days", 1)
+    .set("hours", 18),
+
+  "over 3 jaar, 2 maanden en 2 weken op dinsdag": getDate()
+    .set("years", 3)
+    .set("months", 2)
+    .set("weeks", 2)
+    .set("days", 2),
+
+  "over 2 weken op woensdag om 4 uur": getDate()
+    .set("weeks", 2)
+    .set("days", 3)
+    .set("hours", 4),
+
+  "over 1 maand en 2 weken op donderdag 5 uur ‘s middags": getDate()
+    .set("months", 1)
+    .set("date", 14)
+    .set("days", 4)
+    .set("hours", 17),
+
+  "over 2 weken om 6 uur": getDate().set("weeks", 2).set("hours", 6),
+
+  "vrijdag nacht over 3 weken": getDate()
+    .set("weeks", 3)
+    .set("days", 5)
+    .set("hours", 0),
+
+  "op zaterdag over 2 weken en 2 en een half uur": getDate()
+    .set("weeks", 2)
+    .set("days", 6)
+    .set("hours", 2)
+    .set("minutes", 30),
+
+  "op zondag over 2 maanden": getDate().set("months", 3).set("days", 7),
+
+  "dinsdag over 2 jaren en 3 maanden en 2 weken en anderhalf uur": getDate()
+    .set("years", 2)
+    .set("months", 3)
+    .set("weeks", 2)
+    .set("days", 2)
+    .set("hours", 1)
+    .set("minutes", 30),
+
+  "over 2 jaar in de vierde week van september": getDate()
+    .set("years", 2)
+    .set("months", 8)
+    .set("date", 22),
+
+  "in 2 jaar op de derde woensdag": getDate()
+    .set("years", 2)
+    .set("months", 0)
+    .set("date", 17)
+    .set("days", 3),
+
+  "over 2 maanden in de derde week op maandag avond": getDate()
+    .set("months", 2)
+    .set("date", 15)
+    .set("days", 1)
+    .set("hours", 18),
+};
+
+const input = `over 3 uur
+over 10 minuten
+over een kwartier
+over een half uur
+over drie kwartier
+in een uur
+over 2 dagen
+over 6 maanden
+over 10 jaar
+in 5 minuten
+in anderhalf uur
+over 2 jaar, 3 maanden en 2 weken, 1 dag, 4 uur en 30 minuten
+over 1 jaar en 2 maanden
+over 3 dagen en 6 en een half uur
+over 3 weken
+over een maand en 1 week
+in 2 weken op maandagavond
+over 3 jaar, 2 maanden en 2 weken op dinsdag
+over 2 weken op woensdag om 4 uur
+over 1 maand en 2 weken op donderdag 5 uur ‘s middags
+over 2 weken om 6 uur
+vrijdag nacht over 3 weken
+op zaterdag over 2 weken en 2 en een half uur
+op zondag over 2 maanden
+dinsdag over 2 jaren en 3 maanden en 2 weken en anderhalf uur
+over 2 jaar in de vierde week van september
+in 2 jaar op de derde woensdag
+over 2 maanden in de derde week op maandag avond`;
+
+for (const [input, expectedDate] of Object.entries(tests)) {
+  const result = guesser.guess(input);
+
+  if (result === undefined) {
+    console.log(`NOT IMPLEMENTED: ${input}`);
+
+    continue;
+  }
+
+  const pass = result.unix() === expectedDate.unix();
+
+  if (!pass) {
+    const message = `FAIL: "${input}", expected: ${expectedDate.toDate().toLocaleString()}, got: ${result.toDate().toLocaleString()}`;
+
+    console.log(message);
+  }
 }
